0716f4eeb9a5c6e277d3c8768e12ff834f82d7fb
Ioan-Razvan Stanciu <ionutstanciu24@gmail.com>
2024-10-27 23:12:11 +0200

    am modificat pagina de overview

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -8,6 +8,7 @@ import {
 import { AddForm, AddFormDatePicker } from "./AddForm";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Panel } from "primereact/panel";
 
 const Add = () => {
   const [ziCheckIn, setZiCheckIn] = useState(new Date());
@@ -208,58 +209,265 @@ const Add = () => {
   };
 
   return (
-    <ComponentContainer>
-      <AddContainer>
-        <GlobalStyles />
-        {Object.keys(inputObject).map((el, index) => {
-          if (el === "zi_check_in" || el === "zi_check_out") {
-            return (
-              <AddFormDatePicker
-                key={index}
-                name={el}
-                selected={el === "zi_check_in" ? ziCheckIn : ziCheckOut}
-                handleChange={(date) => {
-                  if (el === "zi_check_in") {
-                    setZiCheckIn(date);
-                  } else {
-                    setZiCheckOut(date);
-                  }
-                  handleDateChange(date, el);
-                }}
-                error={
-                  error[el] ||
-                  (el === "zi_check_in" && !ziCheckIn) ||
-                  (el === "zi_check_out" && !ziCheckOut)
-                    ? `You must select a day of ${
-                        el === "zi_check_in" ? "check-in" : "check-out"
-                      }`
-                    : undefined
-                }
-              />
-            );
-          } else {
-            return (
-              <AddForm
-                key={index}
-                name={el}
-                type={
-                  el === "nr_zile_concediu" ||
-                  el === "nr_pers" ||
-                  el === "pret_sejur"
-                    ? "number"
-                    : "text"
-                }
-                value={inputObject[el]}
-                handleChange={handleChange}
-                error={error[el]}
-              />
-            );
-          }
-        })}
-        <AddButton onClick={handleSubmit}>Submit</AddButton>
-        <ToastContainer />
-      </AddContainer>
-    </ComponentContainer>
+    // <ComponentContainer>
+    //   <AddContainer>
+    //     <GlobalStyles />
+    //     {Object.keys(inputObject).map((el, index) => {
+    //       if (el === "zi_check_in" || el === "zi_check_out") {
+    //         return (
+    //           <AddFormDatePicker
+    //             key={index}
+    //             name={el}
+    //             selected={el === "zi_check_in" ? ziCheckIn : ziCheckOut}
+    //             handleChange={(date) => {
+    //               if (el === "zi_check_in") {
+    //                 setZiCheckIn(date);
+    //               } else {
+    //                 setZiCheckOut(date);
+    //               }
+    //               handleDateChange(date, el);
+    //             }}
+    //             error={
+    //               error[el] ||
+    //               (el === "zi_check_in" && !ziCheckIn) ||
+    //               (el === "zi_check_out" && !ziCheckOut)
+    //                 ? `You must select a day of ${
+    //                     el === "zi_check_in" ? "check-in" : "check-out"
+    //                   }`
+    //                 : undefined
+    //             }
+    //           />
+    //         );
+    //       } else {
+    //         return (
+    //           <AddForm
+    //             key={index}
+    //             name={el}
+    //             type={
+    //               el === "nr_zile_concediu" ||
+    //               el === "nr_pers" ||
+    //               el === "pret_sejur"
+    //                 ? "number"
+    //                 : "text"
+    //             }
+    //             value={inputObject[el]}
+    //             handleChange={handleChange}
+    //             error={error[el]}
+    //           />
+    //         );
+    //       }
+    //     })}
+    //     <AddButton onClick={handleSubmit}>Submit</AddButton>
+    //     <ToastContainer />
+    //   </AddContainer>
+    // </ComponentContainer>
+    <div className="p-4">
+      <div className="font-bold text-xl mt-[100px]">
+        Overview of the project
+      </div>
+      <div className="p-2">
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Project title"
+          toggleable
+        >
+          <div className="font-bold">Journey Gem</div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Describe the problem that you have noticed"
+          toggleable
+        >
+          <div className="font-bold">
+            Problema pe care am observat-o este timpul pe care il ocupa
+            organizarea de calatorii: in doua sau mai multe persoane. Cautarea
+            biletelor de avion, costurile de a merge cu masina sau de a inchiria
+            o rulota, chiar si cautarea cazarilor este foarte solicitanta mai
+            ales cand doresti sa mergi cu un grup de prieteni, iar dorintele
+            fiecaruia trebuie satisfacute.
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Your solution / value proposition"
+          toggleable
+        >
+          <div className="font-bold">
+            Cu totii stim ca suntem persoane diferite cu nevoi diferite, unora
+            le place sa mearga cu avionul pentru a scurta timpul pana a ajunge
+            la destinatie, iar altora le place sa mearga cu masina sau rulota.
+            De aici am plecat cu ideea de a forma pachete speciale nevoilor tale
+            sau chiar tu poti sa-ti personalizezi vacanta impreuna cu prietenii,
+            familia sau partenerul de viata. Oferim solutii optime cu
+            transparenta 100% pentru preturile de avion (cu bagaj de cala sau
+            fara), in functie de perioada dorita; costurile de carburant daca
+            doresti sa mergi cu masina personala sau daca doresti sa inchiriezi
+            (inclusiv rulote). Cazarile vor fi puse la dispozitia dvs.
+            diversificate, intr-un mediu usor de cautat in functie de dorintele
+            clientilor.
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Customer segment/s"
+          toggleable
+        >
+          <div className="font-bold">
+            → Turisti independenti;
+            <br />
+            → Grupuri;
+            <br />
+            → Familii;
+            <br />
+            → Seniori (pensionari);
+            <br />
+            → Nomazi digitali;
+            <br />→ Luxury travelers
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Competition"
+          toggleable
+        >
+          <div className="font-bold">
+            → Christian Tour (sau orice firma de pachete travel)
+            <br />→ Tryp.com
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Your advantage over the competition"
+          toggleable
+        >
+          <div className="font-bold">
+            Dorim sa introducem toate elementele necesare (cazari, transport -
+            avion, masina, rulota, croaziere, cu o singura destinatie, cu
+            destinatii multiple, pet friendly, asigurare de calatorie sau pretul
+            taxelor de calatorie) pentru fiecare nevoie in parte a
+            utilizatorilor ce doresc sa colaboreze cu noi pentru o vacanta de
+            vis.
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Key metrics"
+          toggleable
+        >
+          <div className="font-bold">
+            → Principalele metrici de performanță ale aplicației JourneyGem:
+            <br />
+            <br />- Numărul de utilizatori activi lunar (MAU): Monitorizarea
+            utilizatorilor activi lunar pentru a evalua nivelul de retenție și
+            satisfacția utilizatorilor.
+            <br />
+            <br />
+            500 - 1.500 de utilizatori activi lunar în primele 3-6 luni.
+            <br />
+            <br />
+            În primele luni, aplicația poate atrage câteva sute până la peste o
+            mie de utilizatori activi, mai ales cu eforturi de marketing și prin
+            crearea unui efect de „word-of-mouth” pozitiv.
+            <br />
+            <br />- Rata de conversie a utilizatorilor: Procentul utilizatorilor
+            care finalizează o rezervare completă, fie că este vorba de cazare,
+            zbor sau închiriere de vehicule.
+            <br />
+            <br />
+            1% - 5% pentru primele luni
+            <br />
+            <br />
+            Un inceput bun ar fi un procent de 2-3%, in primele luni.
+            <br />
+            <br />- Valoarea medie a rezervării: Suma medie cheltuită pe
+            utilizator în cadrul rezervărilor efectuate prin platformă.
+            <br />
+            <br />
+            Pentru început, este realista o valoare medie între 150 și 300 EUR
+            per rezervare.
+            <br />
+            <br />- Rata de retenție: Procentul de utilizatori care revin pentru
+            a planifica noi călătorii după o primă experiență.
+            <br />
+            <br />
+            20% - 30% în primele 3 luni.
+            <br />
+            <br />
+            Rata de retenție poate crește odată cu îmbunătățirea aplicației și
+            cu dezvoltarea unei experiențe personalizate pentru utilizatori.
+            <br />
+            <br />- Feedback și scor de satisfacție al clienților: Calitatea
+            experienței utilizatorilor măsurată prin feedback și rating-uri
+            oferite de aceștia.
+            <br />
+            <br />- Numărul de pachete personalizate create și vândute:
+            Urmărirea popularității funcției de personalizare a călătoriilor,
+            precum și vânzarea acestor pachete.
+            <br />
+            <br />
+            50 - 100 pachete în primele 6 luni.
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Cost structure"
+          toggleable
+        >
+          <div className="font-bold">
+            → Structura de costuri pentru JourneyGem include următoarele
+            componente principale:
+            <br />
+            <br />- Dezvoltare software și mentenanță: Costuri pentru
+            programatori (frontend și backend), design UI/UX, testare, hosting,
+            licențe de software, actualizări și securitate.
+            <br />
+            <br />- Infrastructură și găzduire: Cheltuieli pentru servere, baze
+            de date și managementul infrastructurii cloud necesare pentru
+            funcționarea optimă a aplicației.
+            <br />
+            <br />
+            16.000 - 30.000 EUR/an
+            <br />
+            <br />- Comisioane și parteneriate: Taxe și comisioane pentru
+            parteneriatele cu furnizori de servicii de transport, cazări și
+            platforme de închiriere de vehicule.
+            <br />
+            <br />
+            10.000 - 25.000 EUR/an, în funcție de volumul tranzacțiilor și
+            partenerii aleși.
+          </div>
+        </Panel>
+        <Panel
+          className="mt-4 shadow-lg border-2 border-[#8fbc8f] rounded-tl-[6px] rounded-tr-[6px]"
+          header="Revenue streams"
+          toggleable
+        >
+          <div className="font-bold">
+            → Pentru a asigura profitabilitatea JourneyGem am prevazut
+            urmatoarele surse de venituri:
+            <br />
+            <br />- Comisioane la rezervari: venituri generate de comisioane
+            aplicate fiecarei rezervari, zbor sau inchiriere de vehicule
+            realizate prin platforma.
+            <br />
+            <br />
+            Se percepe un comision de 5% la fiecare rezervare pentru inceput.
+            <br />
+            <br />- Publicitate si promovare pe platforma: posibilitatea de a
+            afisa anunturi sponsorizate de la parteneri.
+            <br />
+            <br />
+            Publicitatea pe platformele de călătorii este de obicei tarifată la
+            un CPC de 0,5 - 2 EUR/click sau un CPM de 10 - 15 EUR pentru
+            afișările anunțurilor.
+            <br />
+            <br />
+            -Pachete premium: posibilitatea de a oferi pachete premium pentru
+            vacante ce includ optiuni speciale.
+          </div>
+        </Panel>
+      </div>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./custom_theme/custom-green-theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
