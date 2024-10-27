66dde4258b6db1495fb52939e0166cf3e0fb4664
Ioan-Razvan Stanciu <ionutstanciu24@gmail.com>
2024-10-27 17:44:53 +0200

    am modificat imaginea de fundal pentur home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,11 +10,7 @@ import HomeButtons from "./HomeButtons";
 function Home() {
   return (
     <HomeContainer>
-      <HomeImageBackground
-        src={
-          "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsJTIwbmF0dXJlfGVufDB8fDB8fHww"
-        }
-      />
+      <HomeImageBackground src={"src/assets/home_photo.png"} />
       <HomeText>Explore The Beauty Of Our World</HomeText>
       <HomeButtonsContainer>
         <HomeButtons />
